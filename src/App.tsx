bd73dcdadb75744fbbf731978d24a50e950b1305
Anna Lebid <annalez16@gmail.com>
2026-05-25 14:13:17 +0300

    feat: cocktails section

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,16 @@ import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 import { SplitText } from "gsap/dist/SplitText";
 import NavBar from "./components/NavBar";
 import Hero from "./components/Hero";
+import Cocktails from "./components/Cocktails";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
-    <main className=" h-screen overflow-hidden">
+    <main className=" h-screen">
       <NavBar />
       <Hero />
+      <Cocktails />
     </main>
   );
 };
