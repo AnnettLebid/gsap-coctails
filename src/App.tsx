902bb7646917d2c6ce879866e465d57a5967c9aa
Anna Lebid <annalez16@gmail.com>
2026-05-25 11:29:20 +0300

    fix: double scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import React from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 import { SplitText } from "gsap/dist/SplitText";
+import NavBar from "./components/NavBar";
+import Hero from "./components/Hero";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
-  return <div className="border border-red-500 flex-center h-[100vh]">App</div>;
+  return (
+    <main className=" h-screen overflow-hidden">
+      <NavBar />
+      <Hero />
+    </main>
+  );
 };
 
 export default App;
